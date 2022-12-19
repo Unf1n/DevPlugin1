cb0baa1bb12a6fcd5bb0587e46802581ac4ee3ce
Unf1n <118728757+Unf1n@users.noreply.github.com>
2022-12-19 15:56:57 +0500

    Demo

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -38,6 +38,8 @@
             this.label2 = new System.Windows.Forms.Label();
             this.textBox3 = new System.Windows.Forms.TextBox();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -45,43 +47,53 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label1.Location = new System.Drawing.Point(444, 195);
+            this.label1.Location = new System.Drawing.Point(28, 16);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(168, 20);
+            this.label1.Size = new System.Drawing.Size(112, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Называние проекта: ";
+            this.label1.Text = "Project Name: ";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label3.Location = new System.Drawing.Point(37, 15);
+            this.label3.Location = new System.Drawing.Point(28, 98);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(64, 20);
+            this.label3.Size = new System.Drawing.Size(67, 20);
             this.label3.TabIndex = 2;
-            this.label3.Text = "Версия";
+            this.label3.Text = "Version:";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label4.Location = new System.Drawing.Point(413, 287);
+            this.label4.Location = new System.Drawing.Point(28, 140);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(113, 20);
+            this.label4.Size = new System.Drawing.Size(112, 20);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Версия Petrel:";
+            this.label4.Text = "Petrel Version:";
             // 
             // comboBox1
             // 
             this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(588, 279);
+            this.comboBox1.Items.AddRange(new object[] {
+            "2013",
+            "2014",
+            "2015",
+            "2016",
+            "2018",
+            "2019",
+            "2020",
+            "2021",
+            "2022"});
+            this.comboBox1.Location = new System.Drawing.Point(194, 142);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(183, 21);
             this.comboBox1.TabIndex = 4;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(3, 284);
+            this.textBox1.Location = new System.Drawing.Point(194, 16);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(183, 20);
             this.textBox1.TabIndex = 7;
@@ -89,19 +101,20 @@
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(514, 172);
+            this.textBox2.Location = new System.Drawing.Point(194, 100);
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(183, 20);
             this.textBox2.TabIndex = 8;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.button1.Location = new System.Drawing.Point(588, 402);
+            this.button1.Location = new System.Drawing.Point(539, 593);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(200, 36);
             this.button1.TabIndex = 9;
-            this.button1.Text = "Создать";
+            this.button1.Text = "Create";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -109,7 +122,7 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label2.Location = new System.Drawing.Point(413, 324);
+            this.label2.Location = new System.Drawing.Point(41, 352);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(46, 20);
             this.label2.TabIndex = 10;
@@ -117,7 +130,7 @@
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(588, 324);
+            this.textBox3.Location = new System.Drawing.Point(207, 354);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(183, 20);
             this.textBox3.TabIndex = 11;
@@ -125,26 +138,46 @@
             // 
             // panel1
             // 
-            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.comboBox1);
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.textBox4);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.label5);
             this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label1);
             this.panel1.Location = new System.Drawing.Point(13, 22);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(291, 278);
+            this.panel1.Size = new System.Drawing.Size(442, 311);
             this.panel1.TabIndex = 12;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(194, 59);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(183, 20);
+            this.textBox4.TabIndex = 13;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(28, 57);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(126, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Product Version:";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 669);
             this.Controls.Add(this.panel1);
-            this.Controls.Add(this.textBox3);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox3);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
@@ -167,6 +200,8 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.TextBox textBox3;
         private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox4;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,8 +7,10 @@ using System.IO;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.LinkLabel;
 
 namespace DevPlugin
 {
@@ -43,6 +45,14 @@ namespace DevPlugin
            
         }
 
+        public bool change (string seek, string text)
+        {
+                if (Regex.IsMatch(text, seek))
+                {
+                   return true;
+                }
+            return false;
+        }
         private void button1_Click(object sender, EventArgs e)
         {
             string[] lines = new string[500];
@@ -52,7 +62,7 @@ namespace DevPlugin
             }
             else
             {
-                string filename = path1 + "\\data\\data.csproj";
+                string filename = path1 + "\\simple-plugin\\TCG.SimplePlugin\\TCG.SimplePlugin.csproj";
                 MessageBox.Show(path1);
                 MessageBox.Show(filename);
 
@@ -69,18 +79,52 @@ namespace DevPlugin
                     lines = list.ToArray();
                 }
 
-
-
+               var guID1 = Guid.NewGuid();
+                string strid = guID1.ToString();
                 for (int i = 1; i < lines.Length; i++)
                 {
-                    if (lines[i].IndexOf("<RootNamespace>") > 0)
-                    {
-                        lines[i] = $"<RootNamespace>TCG.{textBox1.Text}</RootNamespace>";
-                    }
-                    if (lines[i].IndexOf("<AssemblyName>") > 0)
-                    {
-                        lines[i] = $"<AssemblyName>{textBox1.Text}</AssemblyName>";
-                    }
+                    if (change(" <RootNamespace>(\\w+?.\\w+?)</RootNamespace>", lines[i]))
+                        lines[i] = $"<RootNamespace>{textBox1.Text}</RootNamespace>";
+                    if (change(" <AssemblyName>(\\w+?.\\w+?)</AssemblyName>", lines[i]))
+                        lines[i] = $" <AssemblyName>{textBox1.Text}</AssemblyName>";
+                    if (change("<ProductVersion>(\\w+?.\\w+?.\\w+?)</ProductVersion>", lines[i]))
+                        lines[i] = $"<ProductVersion>{textBox4.Text}</ProductVersion>";
+                    if (change("<ProductVersion>(\\w+?.\\w+?.\\w+?)</ProductVersion>", lines[i]))
+                        lines[i] = $"<ProductVersion>{textBox4.Text}</ProductVersion>";
+                    if (lines[i].IndexOf("<ProjectGuid>{034805E2-9B80-4F18-930B-0E789C4B9A8C}</ProjectGuid>") > 0)
+                        lines[i] = $"<ProjectGuid>{{{strid}}}</ProjectGuid>";
+
+                    if (lines[i].IndexOf("<HintPath>C:\\Program Files\\Schlumberger\\Petrel 2013\\Public\\Slb.Ocean.Data.dll</HintPath>") > 0)
+                        lines[i] = $"<HintPath>C:\\Program Files\\Schlumberger\\Petrel {comboBox1.Text}\\Public\\Slb.Ocean.Data.dll</HintPath>";
+
+                    if (lines[i].IndexOf("<HintPath>C:\\Program Files\\Schlumberger\\Petrel 2013\\Public\\Slb.Ocean.Petrel.Geology.dll</HintPath>") > 0)
+                        lines[i] = $"<HintPath>C:\\Program Files\\Schlumberger\\Petrel {comboBox1.Text}\\Public\\Slb.Ocean.Petrel.Geology.dll</HintPath>>";
+
+                    if (lines[i].IndexOf("<HintPath>C:\\Program Files\\Schlumberger\\Petrel 2013\\Public\\Slb.Ocean.Petrel.Modeling.dll</HintPath>") > 0)
+                        lines[i] = $"<HintPath>C:\\Program Files\\Schlumberger\\Petrel {comboBox1.Text}\\Slb.Ocean.Petrel.Modeling.dll</HintPath>";
+
+                    if (lines[i].IndexOf("<HintPath>C:\\Program Files\\Schlumberger\\Petrel 2013\\Public\\Slb.Ocean.Petrel.UI.Controls.dll</HintPath>") > 0)
+                        lines[i] = $"<HintPath>C:\\Program Files\\Schlumberger\\Petrel {comboBox1.Text}\\Public\\Slb.Ocean.Petrel.UI.Controls.dll</HintPath>";
+
+                    if (lines[i].IndexOf("<HintPath>C:\\Program Files\\Schlumberger\\Petrel 2013\\Public\\Slb.Ocean.Petrel.Well.dll</HintPath>") > 0)
+                        lines[i] = $"<HintPath>C:\\Program Files\\Schlumberger\\Petrel {comboBox1.Text}\\Public\\Slb.Ocean.Petrel.Well.dll</HintPath>";
+
+                    if (lines[i].IndexOf("<HintPath>C:\\Program Files\\Schlumberger\\Petrel 2020\\Public\\Slb.Ocean.Units.dll</HintPath>") > 0)
+                        lines[i] = $"<HintPath>C:\\Program Files\\Schlumberger\\Petrel {comboBox1.Text}\\Public\\Slb.Ocean.Units.dll</HintPath>";
+
+
+                    if (lines[i].IndexOf("<HintPath>c:\\program files\\schlumberger\\petrel 2013\\Public\\slb.ocean.core.dll</HintPath>") > 0)
+                        lines[i] = $"<HintPath>c:\\program files\\schlumberger\\petrel {comboBox1.Text}\\Public\\slb.ocean.core.dll</HintPath>";
+
+                    if (lines[i].IndexOf("<HintPath>c:\\program files\\schlumberger\\petrel 2013\\Public\\slb.ocean.petrel.dll</HintPath>") > 0)
+                        lines[i] = $"<HintPath>c:\\program files\\schlumberger\\petrel {comboBox1.Text}\\Public\\slb.ocean.petrel</HintPath>";
+
+                    if (lines[i].IndexOf("<HintPath>c:\\program files\\schlumberger\\petrel 2013\\Public\\Slb.Ocean.Basics.dll</HintPath>") > 0)
+                        lines[i] = $"<HintPath>c:\\program files\\schlumberger\\petrel {comboBox1.Text}\\Public\\Slb.Ocean.Basics.dll</HintPath>";
+
+                    if (lines[i].IndexOf("<HintPath>c:\\program files\\schlumberger\\petrel 2013\\Public\\Slb.Ocean.Geometry.dll</HintPath>") > 0)
+                        lines[i] = $"<HintPath>c:\\program files\\schlumberger\\petrel {comboBox1.Text}\\Public\\Slb.Ocean.Geometry.dll</HintPath>";
+
                 }
 
                 Directory.CreateDirectory(path + '\\' + textBox1.Text);
@@ -104,6 +148,21 @@ namespace DevPlugin
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label7_Click(object sender, EventArgs e)
         {
 
         }
