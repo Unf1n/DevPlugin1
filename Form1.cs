0eaf29f89ff25f0df2205e4d5fa385715ce31598
Unf1n <118728757+Unf1n@users.noreply.github.com>
2022-12-26 15:03:59 +0500

    0

--- Form1.cs
+++ Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Linq;
@@ -75,6 +76,8 @@ namespace DevPlugin
             string[] TCGClines = new string[500];
             string[] linesSLN = new string[500];
             string[] linesAPINI = new string[500];
+            string[] linesTEMP = new string[500];
+
 
 
             if (path == "")
@@ -83,20 +86,30 @@ namespace DevPlugin
             }
             else
             {
+                string PName = "";
+
                 string filenameTCG = path1 + "\\simple-plugin\\TCG.SimplePlugin\\TCG.SimplePlugin.csproj";
                 string filenameSLN = path1 + "\\simple-plugin\\TCG.SimplePlugin.sln";
                 string filenameAPINI = path1 + "\\simple-plugin\\Appinfo.ini";
+                string filenameTEMP = path1 + "\\simple - plugin\\TCG.SimplePlugin\\obj\\Debug\\TempPE\\Properties.Resources.Designer.cs.dll";
+
 
                 TCGClines = WriteF(filenameTCG);
                 linesSLN = WriteF(filenameSLN);
-                linesAPINI= WriteF(filenameAPINI);
+                linesAPINI = WriteF(filenameAPINI);
+                linesTEMP = WriteF(filenameAPINI);
+
 
                 var guID1 = Guid.NewGuid();
                 string strid1 = guID1.ToString();
 
-
-
-                for (int i = 1; i < linesSLN.Length; i++)
+                for(int i = 0; i < textBox1.Text.Length; i ++)
+                {
+                    PName += textBox1.Text[i];
+                    PName += " ";
+                }
+                linesTEMP[30] = $"esources DebuggingModes TCG.{textBox1.Text}.Properties Settings Microsoft.CodeAnalysis AttributeTargets GetObject get_Default get_Assembly     KT C G . {PName}. P r o p e r t i e s . R e s o u r c e s  \u000fA p p I n f o  +L o g o _ T i m a l _ F i n a l 3 2 x 3 2     Ї·є33\u001aґIІВkQN\u001a\bЕ \u0004 \u0001\u0001\b\u0003  \u0001\u0005 \u0001\u0001\u0011\u0011\u0005 \u0001\u0001\u0011\u001d\u0005 \u0002\u0001\u000e\u000e\u0005 \u0001\u0001\u0011=\a\a\u0003\u0002\u00121\u00121\u0006 \u0001\u0012I\u0011M\u0004  \u0012Q\u0006 \u0002\u0001\u000e\u0012Q\u0004\a\u0001\u00125\u0003\a\u0001\u000e\u0006 \u0002\u000e\u000e\u00125\u0005\a\u0002\u001c\u00129\u0006 \u0002\u001c\u000e\u00125\u0004\a\u0001\u0012\u0014\u0006 \u0001\u0012U\u0012U\b·z\\V\u00194а‰\b°?_\u007f\u0011Х";
+                for (int i = 1; i < linesSLN.Length-2; i++)
                 {
                     
                        linesSLN[4] = $"Project(\"{{{Guid.NewGuid().ToString()}}}\") = \"TCG.{textBox1.Text}\", \"TCG.{textBox1.Text}\\TCG.{textBox1.Text}.csproj\", \"{{{strid1}}}\"";
@@ -155,8 +168,10 @@ namespace DevPlugin
                     if (TCGClines[i].IndexOf("<HintPath>c:\\program files\\schlumberger\\petrel 2013\\Public\\Slb.Ocean.Geometry.dll</HintPath>") > 0)
                         TCGClines[i] = $"<HintPath>c:\\program files\\schlumberger\\petrel {comboBox1.Text}\\Public\\Slb.Ocean.Geometry.dll</HintPath>";
 
-                }
+                    TCGClines[178] = $"\"%25OCEAN{comboBox1.Text}HOME%25\\PluginPackager.exe\" / g \"$(TargetPath)\" \"$(ProjectDir)\\plugin.xml\" \"%25OCEAN{comboBox1.Text}HOME_x64%25\\petrel.exe\"";
+                    TCGClines[179] = $"\"%25OCEAN{comboBox1.Text}HOME%25\\PluginPackager.exe\" /m \"$(ProjectDir)\\plugin.xml\" \"%25OCEAN{comboBox1.Text}HOME_x64%25\\petrel.exe\" \"$(TargetDir)\"</PostBuildEvent>";
 
+                }
                 for (int i = 1; i < linesAPINI.Length; i++)
                 {
 
@@ -168,6 +183,7 @@ namespace DevPlugin
                 Directory.CreateDirectory(path + '\\' + textBox1.Text);
                 Directory.CreateDirectory(path + '\\' + textBox1.Text + "\\TCG.Fractures");
 
+                File.AppendAllLines($"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\obj\\Debug\\TempPE\\Properties.Resources.Designer.cs.dll", linesTEMP);
                 File.AppendAllLines($"{path}\\{textBox1.Text}\\TCG.Fractures\\TCG.{textBox1.Text}.csproj",TCGClines);
                 File.AppendAllLines($"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}.sln", linesSLN);
                 File.AppendAllLines($"{path}\\{textBox1.Text}\\Appinfo.ini", linesAPINI);
