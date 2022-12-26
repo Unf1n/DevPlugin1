7a6d484d3f5dec520d88b19f90337d0e7bf648ec
Unf1n <118728757+Unf1n@users.noreply.github.com>
2022-12-26 17:48:28 +0500

    END

--- Form1.cs
+++ Form1.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Data;
 using System.Diagnostics;
 using System.Drawing;
@@ -76,7 +77,16 @@ namespace DevPlugin
             string[] TCGClines = new string[500];
             string[] linesSLN = new string[500];
             string[] linesAPINI = new string[500];
-            string[] linesTEMP = new string[500];
+            string[] linesAssemblyInfo = new string[500];
+            string[] linesResourcesDesigner = new string[500];
+            string[] linesPlugincs = new string[500];
+            string[] linesPlugin = new string[500];
+            string[] linesCSpro = new string[500];
+            string[] linesWorkstep1 = new string[500];
+            string[] linesWorkstep1UI = new string[500];
+            string[] linesWorkstep1UIDesigner = new string[500];
+
+
 
 
 
@@ -86,28 +96,72 @@ namespace DevPlugin
             }
             else
             {
-                string PName = "";
 
                 string filenameTCG = path1 + "\\simple-plugin\\TCG.SimplePlugin\\TCG.SimplePlugin.csproj";
                 string filenameSLN = path1 + "\\simple-plugin\\TCG.SimplePlugin.sln";
                 string filenameAPINI = path1 + "\\simple-plugin\\Appinfo.ini";
-                string filenameTEMP = path1 + "\\simple-plugin\\TCG.SimplePlugin\\obj\\Debug\\TempPE\\Properties.Resources.Designer.cs.dll";
+                string filenameAssemblyInfo = path1 + "\\simple-plugin\\TCG.SimplePlugin\\Properties\\AssemblyInfo.cs";
+                string filenameResourcesDesigner = path1 + "\\simple-plugin\\TCG.SimplePlugin\\Properties\\Resources.Designer.cs";
+
+                string filenamePlugincs = path1 + "\\simple-plugin\\TCG.SimplePlugin\\Plugin.cs";
+                string filenamePlugin = path1 + "\\simple-plugin\\TCG.SimplePlugin\\plugin.xml";
+                string filenameCSpro = path1 + "\\simple-plugin\\TCG.SimplePlugin\\TCG.SimplePlugin.csproj.user";
+                string filenameWorkstep1 = path1 + "\\simple-plugin\\TCG.SimplePlugin\\Workstep1.cs";
+                string filenameWorkstep1UI = path1 + "\\simple-plugin\\TCG.SimplePlugin\\Workstep1UI.cs";
+                string filename1UIDesigner = path1 + "\\simple-plugin\\TCG.SimplePlugin\\Workstep1UI.Designer.cs";
 
 
                 TCGClines = WriteF(filenameTCG);
                 linesSLN = WriteF(filenameSLN);
                 linesAPINI = WriteF(filenameAPINI);
-                linesTEMP = WriteF(filenameTEMP);
+                linesAssemblyInfo = WriteF(filenameAssemblyInfo);
+                linesResourcesDesigner = WriteF(filenameResourcesDesigner);
+                linesPlugincs = WriteF(filenamePlugincs);
+                linesPlugin = WriteF(filenamePlugin);
+                linesCSpro = WriteF(filenameCSpro);
+                linesWorkstep1 = WriteF(filenameWorkstep1);
+                linesWorkstep1UI = WriteF(filenameWorkstep1UI);
+                linesWorkstep1UIDesigner = WriteF(filename1UIDesigner);
+
 
 
                 var guID1 = Guid.NewGuid();
                 string strid1 = guID1.ToString();
 
-                for(int i = 0; i < textBox1.Text.Length; i ++)
-                {
-                    PName += textBox1.Text[i];
-                    PName += " ";
-                }
+                linesAssemblyInfo[7] = $"[assembly: AssemblyTitle(\"TCG.{textBox1.Text}\")]";
+                linesAssemblyInfo[11] = $"[assembly: AssemblyProduct(\"TCG.{textBox1.Text}\")]";
+                linesAssemblyInfo[22] = $"[assembly: Guid(\"{Guid.NewGuid().ToString()}\")]";
+
+                linesResourcesDesigner[41] = $"                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager(\"TCG.{textBox1.Text}.Properties.Resources\", typeof(Resources).Assembly);";
+                linesResourcesDesigner[64] = $"        ///AppName={textBox1.Text}";
+                linesResourcesDesigner[66] = $"        ///Version={textBox4.Text}";
+                linesResourcesDesigner[67] = $"        ///PetrelVersion={comboBox1.Text}.{textBox2.Text}(64-bit).";
+
+                linesPlugincs[5] = $"namespace TCG.{textBox1.Text}";
+                linesPlugincs[11] = $"            get {{ return \"{textBox4.Text}\"; }}";
+                linesPlugincs[56] = $"            get {{ return \"TCG.{textBox1.Text}\"; }}";
+
+                DateTime today = new DateTime();
+
+                linesPlugin[3] = $"    <Name>TCG.{textBox1.Text}</Name>";
+                linesPlugin[5] = $"    <PluginTypeName>TCG.{textBox1.Text}.Plugin, TCG.{textBox1.Text}, Version=1.0.3.0, Culture=neutral, PublicKeyToken=9de9635fa700ae8a</PluginTypeName>";
+                linesPlugin[7] = $"    <PluginId>://TCG.{textBox1.Text}.Plugin/1.0.3.0</PluginId>";
+                linesPlugin[9] = $"    <ReleaseDate>{today.ToString("yyyy/MM/dd")}</ReleaseDate>";
+                linesPlugin[11] = $"    <AppVersion>{comboBox1.Text}</AppVersion>";
+                linesPlugin[13] = $"      <Module TypeName=\"TCG.{textBox1.Text}.Module, TCG.{textBox1.Text}, Version=1.0.3.0, Culture=neutral, PublicKeyToken=9de9635fa700ae8a\" />";
+                linesPlugin[19] = $"      <Assembly Name=\"TCG.{textBox1.Text}, Version=1.0.3.0, Culture=neutral, PublicKeyToken=9de9635fa700ae8a\" />";
+
+                linesCSpro[4] = $"    <StartProgram>c:\\program files\\schlumberger\\Petrel {comboBox1.Text}\\petrel.exe</StartProgram>";
+                linesCSpro[5] = $"    <StartWorkingDirectory>c:\\program files\\schlumberger\\Petrel {comboBox1.Text}\\</StartWorkingDirectory>";
+                linesCSpro[8] = $"    <ReferencePath>C:\\Program Files\\Schlumberger\\Petrel {comboBox1.Text}\\Public\\;D:\\svn\\svn_kz\\Tools\\bin\\</ReferencePath>";
+
+                linesWorkstep1[9] = $"namespace TCG.{textBox1.Text}";
+                linesWorkstep1[44] = $"				return \"{Guid.NewGuid().ToString()}\";";
+
+                linesWorkstep1UI[8] = $"namespace TCG.{textBox1.Text}";
+
+                linesWorkstep1UIDesigner[0] = $"namespace TCG.{textBox1.Text}";
+
                 for (int i = 0; i < linesSLN.Length; i++)
                 {
                     
@@ -182,14 +236,33 @@ namespace DevPlugin
                 Directory.CreateDirectory($"{path}\\{textBox1.Text}");
                 Directory.CreateDirectory($"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}");
                 Directory.CreateDirectory($"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\Properties");
+                Directory.CreateDirectory($"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\Resources");
+
                 File.Copy(@"C:\Users\Unf1n\Desktop\Unf1n\Проекты\DevPlugin\DevPlugin\bin\Debug\simple-plugin\TCG.SimplePlugin\Properties\Resources.resx", $"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\Properties\\Resources.resx",true);
+                File.Copy(@"C:\Users\Unf1n\Desktop\Unf1n\Проекты\DevPlugin\DevPlugin\bin\Debug\simple-plugin\TCG.SimplePlugin\Properties\Settings.Designer.cs", $"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\Properties\\Settings.Designer.cs", true);
+                File.Copy(@"C:\Users\Unf1n\Desktop\Unf1n\Проекты\DevPlugin\DevPlugin\bin\Debug\simple-plugin\TCG.SimplePlugin\Properties\Settings.settings", $"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\Properties\\Settings.settings", true);
+                File.Copy(@"C:\Users\Unf1n\Desktop\Unf1n\Проекты\DevPlugin\DevPlugin\bin\Debug\simple-plugin\TCG.SimplePlugin\app.config", $"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\app.config", true);
+                File.Copy(@"C:\Users\Unf1n\Desktop\Unf1n\Проекты\DevPlugin\DevPlugin\bin\Debug\simple-plugin\TCG.SimplePlugin\Fractures.key.snk", $"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\Fractures.key.snk", true);
+                File.Copy(@"C:\Users\Unf1n\Desktop\Unf1n\Проекты\DevPlugin\DevPlugin\bin\Debug\simple-plugin\TCG.SimplePlugin\Module.cs", $"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\Module.cs", true);
+                File.Copy(@"C:\Users\Unf1n\Desktop\Unf1n\Проекты\DevPlugin\DevPlugin\bin\Debug\simple-plugin\TCG.SimplePlugin\newkey.snk", $"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\newkey.snk", true);
+                File.Copy(@"C:\Users\Unf1n\Desktop\Unf1n\Проекты\DevPlugin\DevPlugin\bin\Debug\simple-plugin\TCG.SimplePlugin\Workstep1UI.resx", $"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\Workstep1UI.resx", true);
+
+                File.Copy(@"C:\Users\Unf1n\Desktop\Unf1n\Проекты\DevPlugin\DevPlugin\bin\Debug\simple-plugin\TCG.SimplePlugin\Resources\Logo_Timal_Final32x32.png", $"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\Resources\\Logo_Timal_Final32x32.png", true);
 
 
-                File.AppendAllLines($"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\obj\\Debug\\TempPE\\Properties.Resources.Designer.cs.dll", linesTEMP);
+                File.AppendAllLines($@"{path}\{textBox1.Text}\TCG.{textBox1.Text}\Properties\AssemblyInfo.cs", linesAssemblyInfo);
+                File.AppendAllLines($@"{path}\{textBox1.Text}\TCG.{textBox1.Text}\Properties\Resources.Designer.cs", linesResourcesDesigner);
+
+                File.AppendAllLines($@"{path}\{textBox1.Text}\TCG.{textBox1.Text}\Plugin.cs", linesPlugincs);
+                File.AppendAllLines($@"{path}\{textBox1.Text}\TCG.{textBox1.Text}\plugin.xml", linesPlugin);
+                File.AppendAllLines($@"{path}\{textBox1.Text}\TCG.{textBox1.Text}\TCG.SimplePlugin.csproj.user", linesCSpro);
+                File.AppendAllLines($@"{path}\{textBox1.Text}\TCG.{textBox1.Text}\Workstep1.cs", linesWorkstep1);
+                File.AppendAllLines($@"{path}\{textBox1.Text}\TCG.{textBox1.Text}\Workstep1UI.cs", linesWorkstep1UI);
+                File.AppendAllLines($@"{path}\{textBox1.Text}\TCG.{textBox1.Text}\Workstep1UI.Designer.cs", linesWorkstep1UIDesigner);
+
                 File.AppendAllLines($"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\TCG.{textBox1.Text}.csproj",TCGClines);
                 File.AppendAllLines($"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}.sln", linesSLN);
                 File.AppendAllLines($"{path}\\{textBox1.Text}\\Appinfo.ini", linesAPINI);
-                File.Create($"{path}\\{textBox1.Text}\\TCG.{textBox1.Text}\\TCG.Fractures.csproj.backup_13.08.2015 22-17-53");
 
 
             }
